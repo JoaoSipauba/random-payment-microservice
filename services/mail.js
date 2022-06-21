12e62f9ddcb02a971afa314c5d19dbfeec9731be
JoaoSipauba <joaosipauba@hotmail.com>
2022-06-21 01:39:30 -0300

    enviando emails de forma sincrona

--- services/mail.js
+++ services/mail.js
@@ -15,50 +15,48 @@ var transporter = nodemailer.createTransport({
     }
 });
 
-function log(message) {
-    var now = new Date()
-    var log = `${now.toUTCString()} - ${message}`;
 
-    console.log(log);
-}
-
-async function sendMail(msg) {
-    let info = await transporter.sendMail({
-        from: msg.from,
-        to: msg.to,
-        subject: msg.subject,
-        text: msg.text,
-        html: msg.html,
-    });
-
-    log('Mail sent')
-}
-
-amqp.connect('amqp://localhost', function(error0, connection) {
-  if (error0) {
-    throw error0;
-  }
-  connection.createChannel(function(error1, channel) {
-    if (error1) {
-      throw error1;
-    }
+amqp.connect('amqp://localhost', (error0, connection) => {
+  connection.createChannel((error1, channel) => {
     var queue = 'mail-queue';
 
+    channel.prefetch(1);
     channel.assertQueue(queue, {
       durable: true
     });
 
-    channel.consume(queue, async function(msg) {
-        msg = JSON.parse(msg.content.toString());
-        console.log(msg);
-        try{
-            await sendMail(msg);
-        }
-        catch(error){
-            console.log(error);
-        }
-    }, {
-        noAck: true
+    function log(message) {
+      var now = new Date()
+      var log = `${now.toUTCString()} - ${message}`;
+  
+      console.log(log);
+    }
+    
+    async function sendMail(msg) {
+        let info = await transporter.sendMail({
+            from: msg.from,
+            to: msg.to,
+            subject: msg.subject,
+            text: msg.text,
+            html: msg.html,
+        });
+    
+        log('Mail sent')
+    
+        return info
+    }
+    
+    const processMessage = async (msg) => {
+      try {
+        await sendMail(JSON.parse(msg.content.toString()));
+        channel.ack(msg);
+      } catch(error) {
+        channel.reject(msg)
+        console.error(error);
+      }
+    }
+    channel.consume(queue, processMessage, {
+        noAck: false
     });
   });
 });
